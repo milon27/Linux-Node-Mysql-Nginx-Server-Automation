c525680e2d739a85293e3c84f3cc33923890a594
milon27 <mdjahidulislammilon@gmail.com>
2021-11-20 17:20:35 +0600

    organize

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Linux-Node-Mysql-Nginx-Server-Automation
+
+
+- run setup.sh
+- check after-setup.txt file.
+
+
+> finally ck all the steps are done or not.
+  1. check all-setps-shortcut.txt file
+
+> some helper file.
+  1. for nginx server setup [nginx-server-block.txt]
--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 #########################
 # Variable
 ##########################
+TIMEZONE="Asia/Dhaka"
 
 USERNAME="new_user_name"
 PASSWORD="new_user_password"
@@ -21,11 +22,14 @@ DB="db_name"
 apt update
 apt dist-upgrade
 
+#set your local timezone
+timedatectl set-timezone $TIMEZONE
+
 
 echo "creating a sudo user"
 
 #1 enable auto update
-#dpkg-reconfigure --priority=low unattended-upgrades
+dpkg-reconfigure --priority=low unattended-upgrades
 
 #2 create a user
 useradd -m -p $(openssl passwd -1 "$PASSWORD") -s /bin/bash -G sudo "$USERNAME"
@@ -37,12 +41,13 @@ useradd -m -p $(openssl passwd -1 "$PASSWORD") -s /bin/bash -G sudo "$USERNAME"
 cd $HOEM_DIR && mkdir "$HOEM_DIR/.ssh" && chmod 700 "$HOEM_DIR/.ssh"
 cp /root/.ssh/authorized_keys "$HOEM_DIR/.ssh"
 sudo chown "$USERNAME":"$USERNAME" .ssh
+sudo chown "$USERNAME":"$USERNAME" .ssh/authorized_keys
 
 #  remove password login (will do later)
 sudo nano /etc/ssh/sshd_config
 # *. permitRootLogin yes -> no
 # *. passwordAuthentication yes->no
-
+sudo systemctl restart sshd
 
 ########################
 # Start Installation
@@ -78,6 +83,13 @@ create database $DB;
 MYSQL_SCRIPT
 echo "MySQL user&Db created."
 
+#create a folder where all mysql backup will store.
+mkdir $HOEM_DIR/backup
+sudo chown "$USERNAME":"$USERNAME" $HOEM_DIR/backup
+#create a folder where mmenv.txt and action-runner will store.
+mkdir $HOEM_DIR/application
+sudo chown "$USERNAME":"$USERNAME" $HOEM_DIR/application
+
 
 #6. install pm2
 sudo npm install pm2@latest -g
@@ -85,10 +97,32 @@ sudo npm install pm2@latest -g
 #7. install nginx
 sudo apt install nginx
 
+#7.1 make /var/www/html public to upload using scp. (only needed when root login is disabled)
+chmod 777 /var/www/html
+
 #8. install ssl certbot & python3
 sudo apt install certbot python3-certbot-nginx
 
 # other things to do.
+  # - pm2 startup
+  # - to remove pm2 startup :  pm2 unstartup systemd
   # - setup domain .
   # - create sub domain if you want .
-  # - setup ssl .
+  # - install ssl .
+  # - setup nginx
+ 
+  # - create mmenv.txt file inside ~/applicaiton/mmenv.txt
+  # - ci/cid create action-runner inside ~/applicaiton/
+  # - api will run on port 2727, setup nginx to reverse proxy
+        # - proxy_pass http://localhost:2727;
+  # - turn off port 2727 from digital ocean firewall
+  # - - ------------------------------------- - -
+  # - - ---Transfer db from another droplet (optional)------ - -
+  # - - ------------------------------------- - -
+
+  # - setup react + gzip on nginx
+  # - upload react files in the /var/www/html folder
+  # - enable gzip in nginx[sudo nano /etc/nginx/nginx.conf] -> sudo systemctl restart nginx
+
+  # - add 3 cron tab. certbot, daily db backup, monthly db delete.[after sestup file.txt]
+  
